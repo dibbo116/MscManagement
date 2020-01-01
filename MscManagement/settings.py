c920ab2ef9ec4c992222ffd8f3084fec6abf1fc1
Dibbo Barua <Dibbo.barua16@gmail.com>
2020-01-02 02:50:55 +0600

    updated settings

--- MscManagement/settings.py
+++ MscManagement/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2h1jc$2sg)3+ehpci^=#5lrai3yn$)=883s(-lsm-+kfn3nzb9'
-
+#SECRET_KEY = '2h1jc$2sg)3+ehpci^=#5lrai3yn$)=883s(-lsm-+kfn3nzb9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mscmanagement.herokuapp.com']
 
 
 # Application definition
