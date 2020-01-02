47213afdef3ad259247dd7531683a6609f55eb2b
Dibbo Barua <Dibbo.barua16@gmail.com>
2020-01-02 08:10:26 +0600

    updatation

--- MscManagement/settings.py
+++ MscManagement/settings.py
@@ -18,14 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2h1jc$2sg)3+ehpci^=#5lrai3yn$)=883s(-lsm-+kfn3nzb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mscmanagement.herokuapp.com']
 
 
 # Application definition
@@ -71,7 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MscManagement.wsgi.application'
 DATABASES = {
-      
+
         'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'MscManagement',
