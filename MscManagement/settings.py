4b2250ceb097bfba9fc7a594c22ab8de8da0526f
Dibbo Barua <Dibbo.barua16@gmail.com>
2020-01-02 23:34:54 +0600

    sssd

--- MscManagement/settings.py
+++ MscManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
-ALLOWED_HOSTS = ['mscruet.herokuapp.c om']
+ALLOWED_HOSTS = ['mscruet.herokuapp.com']
 
 
 # Application definition
